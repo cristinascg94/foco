54c60a48d5ed0c5d5459807c45aa4a976f2126a7
PacoBermudez <fsbermudezg@gmail.com>
2024-10-04 22:25:39 +0200

    Nuevo requi

--- focoProject/build_files.sh
+++ focoProject/build_files.sh
@@ -1,17 +1,17 @@
 echo "Iniciando el proceso de construcción..."
 echo "Versión de Python instalada:"
-python3 --version
+python --version
 
 # Instalar dependencias
 echo "Instalando dependencias..."
-python3 -m pip install -r requirements.txt
+python -m pip install -r requirements.txt
 
 # Realizar migraciones
 echo "Aplicando migraciones..."
-python3 manage.py migrate
+python manage.py migrate
 
 # Recoger archivos estáticos
 echo "Recogiendo archivos estáticos..."
-python3 manage.py collectstatic
+python manage.py collectstatic
 
 echo "Proceso de construcción completado."
