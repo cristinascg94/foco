c693873b5aac769ee937cd9d6282340f9376150e
PacoBermudez <fsbermudezg@gmail.com>
2024-10-04 21:36:39 +0200

    Solucion pro

--- focoProject/focoProject/settings.py
+++ focoProject/focoProject/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ["SECRET_KEY_DJANGO"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+# ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 # Or * to allow all
 ALLOWED_HOSTS = ['*']
 
