be1f18836bbf262d4730fe0d3e74eb0e29c6bad8
PacoBermudez <fsbermudezg@gmail.com>
2024-10-05 12:11:46 +0200

    cambios admin 1

--- focoProject/focoProject/urls.py
+++ focoProject/focoProject/urls.py
@@ -6,9 +6,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('votacion/',  include('votacion.urls')),
-    path('', admin.site.urls),
+    path('admin', admin.site.urls),
 ]
 
 # add at the last
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+#urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
